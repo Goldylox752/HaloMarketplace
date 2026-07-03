b89e407c9f2c65056ee3e3a9dc9398cc5ed44d63
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-07-03 09:58:45 -0600

    Create password.js

---
+++ utils/password.js
@@ -0,0 +1,39 @@
+// ==========================================
+// Password Utility
+// ==========================================
+
+const bcrypt = require("bcrypt");
+
+const SALT_ROUNDS = 12;
+
+async function hash(password) {
+
+    return await bcrypt.hash(
+
+        password,
+
+        SALT_ROUNDS
+
+    );
+
+}
+
+async function compare(password, hash) {
+
+    return await bcrypt.compare(
+
+        password,
+
+        hash
+
+    );
+
+}
+
+module.exports = {
+
+    hash,
+
+    compare
+
+};
