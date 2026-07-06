5a3c00a682dc93d610a30a114b504272078d7edc
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-07-06 10:05:02 -0600

    Add brandController with brand-related functions

---
+++ routes/brandController.js
@@ -0,0 +1,20 @@
+// controllers/brandController.js
+
+module.exports = {
+
+    // Public
+    getBrands,
+    getBrandById,
+    getBrandBySlug,
+    getFeaturedBrands,
+    getPopularBrands,
+    getBrandProducts,
+
+    // Admin
+    createBrand,
+    updateBrand,
+    deleteBrand,
+    toggleFeatured,
+    toggleStatus
+
+};
